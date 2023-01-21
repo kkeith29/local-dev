b0a995e544baa60b9353ab58382a95cd77eb9fa6
Kyle Keith <3694596+kkeith29@users.noreply.github.com>
2023-01-20 23:22:02 -0600

    Update hop config
    - Reorder configure() and keymaps() module methods to match the order they are called
    
    - Move <leader>h to <leader>hc to prevent issues with character searches triggering other keybindings

--- nvim/lua/user/configs/movement.lua
+++ nvim/lua/user/configs/movement.lua
@@ -6,6 +6,12 @@ function M.install(use)
     use 'christoomey/vim-tmux-navigator'
 end
 
+function M.configure()
+    require('hop').setup()
+
+    vim.g.tmux_navigator_no_mappings = 1
+end
+
 function M.keymaps()
     local km = require('user.keymap')
 
@@ -17,22 +23,16 @@ function M.keymaps()
 
     -- Setup Hop
     local hop = require('hop')
-    km.map('<leader>h', function() hop.hint_char1({ current_line_only = true }) end, '[H]op to char within line - Improved f/F')
+    km.map('<leader>hc', function() hop.hint_char1({ current_line_only = true }) end, '[H]op to [C]har within line - Improved f/F')
 
-    km.nnoremap('<leader>hw', function() hop.hint_words() end, '[h]op to any [w]ord in document')
-    km.nnoremap('<leader>hW', function() hop.hint_words({ multi_windows = true }) end, '[h]op to any [W]ord in any window')
+    km.nnoremap('<leader>hw', function() hop.hint_words() end, '[H]op to any [w]ord in document')
+    km.nnoremap('<leader>hW', function() hop.hint_words({ multi_windows = true }) end, '[H]op to any [W]ord in any window')
 
-    km.nnoremap('<leader>hl', function() hop.hint_lines() end, '[h]op to any [l]ine in document')
-    km.nnoremap('<leader>hL', function() hop.hint_lines({ multi_windows = true }) end, '[h]op to any [L]ine in any window')
+    km.nnoremap('<leader>hl', function() hop.hint_lines() end, '[H]op to any [l]ine in document')
+    km.nnoremap('<leader>hL', function() hop.hint_lines({ multi_windows = true }) end, '[H]op to any [L]ine in any window')
 
-    km.nnoremap('<leader>hs', function() hop.hint_patterns() end, '[h]op to pattern [s]earch in document')
-    km.nnoremap('<leader>hS', function() hop.hint_patterns({ multi_windows = true }) end, '[h]op to pattern [S]earch in any window')
-end
-
-function M.configure()
-    require('hop').setup()
-
-    vim.g.tmux_navigator_no_mappings = 1
+    km.nnoremap('<leader>hs', function() hop.hint_patterns() end, '[H]op to pattern [s]earch in document')
+    km.nnoremap('<leader>hS', function() hop.hint_patterns({ multi_windows = true }) end, '[H]op to pattern [S]earch in any window')
 end
 
 return M
