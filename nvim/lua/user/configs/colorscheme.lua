f81565cfdf2f2432b45e9fbff271aef478488265
Kyle Keith <3694596+kkeith29@users.noreply.github.com>
2023-01-20 23:08:54 -0600

    Update colorscheme to carbonfox
    - Add custom highlights for DAP signs

--- nvim/lua/user/configs/colorscheme.lua
+++ nvim/lua/user/configs/colorscheme.lua
@@ -1,17 +1,21 @@
 local M = {
-    enabled = true
+    enabled = true,
+    priority = 85
 }
 
 function M.install(use)
-    use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+    use 'EdenEast/nightfox.nvim'
 end
 
 function M.options()
     vim.o.termguicolors = true
-    vim.cmd [[colorscheme onedark]]
+    vim.cmd [[colorscheme carbonfox]]
 
-    local colors = require('onedark.colors')
-    vim.api.nvim_set_hl(0, 'DapUIBreakpointSign', {fg = colors.red})
+    local cf = require('nightfox.palette.carbonfox').palette
+
+    vim.api.nvim_set_hl(0, 'DapBreakpointSign', {fg = cf.red.base})
+    vim.api.nvim_set_hl(0, 'DapBreakpointConditionalSign', {fg = cf.orange.base})
+    vim.api.nvim_set_hl(0, 'DapStoppedSign', {fg = cf.green.bright})
 end
 
 return M
