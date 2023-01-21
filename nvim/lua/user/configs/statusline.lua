078f5d3dd50422c9634d694a6085635f2b4126f1
Kyle Keith <3694596+kkeith29@users.noreply.github.com>
2023-01-20 23:27:30 -0600

    Update status line plugin config
    - Change to carbonfox theme
    
    - Ignore changing while in dapui related buffers
    
    - Reorder configure() and options() module methods to match the order they are called

--- nvim/lua/user/configs/statusline.lua
+++ nvim/lua/user/configs/statusline.lua
@@ -6,20 +6,25 @@ function M.install(use)
     use 'nvim-lualine/lualine.nvim' -- Fancier statusline
 end
 
-function M.options()
-    vim.o.laststatus = 3
-end
-
 function M.configure()
     -- See `:help lualine.txt`
     require('lualine').setup {
         options = {
-            theme = 'onedark',
+            theme = 'carbonfox',
             disabled_filetypes = {
                 statusline = {'neo-tree'}
+            },
+            ignore_focus = {
+                'dapui_watches', 'dapui_breakpoints',
+                'dapui_scopes', 'dapui_console',
+                'dapui_stacks', 'dap-repl'
             }
         }
     }
+end
+
+function M.options()
+    vim.o.laststatus = 3
 end
 
 return M
