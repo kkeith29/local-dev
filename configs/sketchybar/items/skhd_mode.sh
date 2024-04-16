7dc65d31c2fa598eab515a0c8151e8068616479b
Kyle Keith <3694596+kkeith29@users.noreply.github.com>
2024-04-16 15:31:39 -0500

    Update Sketchybar, cleanup SKHD config, integrate SKHD modes into Sketchybar

---
+++ configs/sketchybar/items/skhd_mode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sketchybar --add item skhd_mode right \
+           --set skhd_mode      drawing=off \
+                                background.color=$ACCENT_COLOR \
+                                icon.color=$BAR_COLOR \
+                                label.color=$BAR_COLOR
---
+++ dotfiles/.zshenv
@@ -0,0 +1,5 @@
+# used to give variables to SKHD which doesn't use interactive shell to execute scripts
+
+export FIREFOX_CONTAINER_1=
+export FIREFOX_CONTAINER_2=
+export FIREFOX_CONTAINER_3=
